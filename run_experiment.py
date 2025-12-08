e7caeaaffbe599463d197d0cfc441639a7643045
Sophia Xia <sx2364@columbia.edu>
2025-12-08 08:11:16 +0000

    start with adding mesh tree

--- run_experiment.py
+++ run_experiment.py
@@ -1,3 +1,4 @@
+import trimesh
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import PoseStamped, Point, Quaternion
@@ -8,6 +9,12 @@ from moveit_msgs.msg import (
     PositionConstraint,
     OrientationConstraint,
     PlanningScene,
+    CollisionObject,
+)
+from shape_msgs.msg import (
+    Mesh,
+    MeshTriangle,
+    SolidPrimitive,
 )
 from builtin_interfaces.msg import Duration
 from shape_msgs.msg import SolidPrimitive
@@ -15,13 +22,64 @@ from rclpy.action import ActionClient
 from visualization_msgs.msg import Marker, MarkerArray
 from math import sqrt
 
-X = 0.1
-Y = 0.72
-Z = 0.3
-OX = sqrt(0.5)
+X = -0.1
+Y = 0.80
+Z = 0.2
+OX = -sqrt(0.5)
 OY = 0.0
 OZ = 0.0
 OW = sqrt(0.5)
+
+class AddTree(Node):
+    def __init__(self):
+        super().__init__("add_tree")
+
+        self.scene_pub = self.create_publisher(
+            PlanningScene,
+            'planning_scene',
+            10
+        )
+
+        # Load mesh using trimesh
+        mesh_path = "./DATA/denoised_mesh.stl"
+        mesh_model = trimesh.load(mesh_path)
+
+        # Convert mesh into ROS message
+        ros_mesh = Mesh()
+        for face in mesh_model.faces:
+            tri = MeshTriangle(vertex_indices=[int(face[0]), int(face[1]), int(face[2])])
+            ros_mesh.triangles.append(tri)
+
+        for vertex in mesh_model.vertices:
+            ros_mesh.vertices.append(Point(
+                x=float(vertex[0]),
+                y=float(vertex[1]),
+                z=float(vertex[2])
+            ))
+
+        # CollisionObject
+        obj = CollisionObject()
+        obj.id = "tree_mesh"
+        obj.header.frame_id = "world"
+
+        pose = PoseStamped()
+        pose.header.frame_id = "world"
+        pose.pose.position.x = 0.5
+        pose.pose.position.y = 0.0
+        pose.pose.position.z = 0.2
+        pose.pose.orientation.w = 1.0
+
+        obj.meshes.append(ros_mesh)
+        obj.mesh_poses.append(pose.pose)
+        obj.operation = CollisionObject.ADD
+
+        # Planning Scene update
+        scene = PlanningScene()
+        scene.is_diff = True
+        scene.world.collision_objects.append(obj)
+
+        self.get_logger().info("Publishing mesh object to planning_scene")
+        self.scene_pub.publish(scene)
 
 def make_sphere(x, y, z, w, scale=0.25):
     sphere = Marker()
@@ -116,6 +174,9 @@ class MoveItPoseGoal(Node):
 
 def main():
     rclpy.init()
+    tree = AddTree()
+    rclpy.spin_once(tree, timeout_sec=2.0)
+
     node = MoveItPoseGoal()
     node.send_goal()
     marker_pub = node.create_publisher(MarkerArray, "/constraint_markers", 10)
