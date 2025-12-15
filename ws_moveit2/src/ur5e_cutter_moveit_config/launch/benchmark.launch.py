979b1f2097a3811c85718b64408df553c8d1b374
sxia1 <sx2364@columbia.edu>
2025-12-15 00:49:32 -0500

    preliminary benchmarking code

---
+++ ws_moveit2/src/ur5e_cutter_moveit_config/launch/benchmark.launch.py
@@ -0,0 +1,75 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from moveit_configs_utils import MoveItConfigsBuilder
+from launch_param_builder import ParameterBuilder
+
+def generate_launch_description():
+
+    moveit_ros_benchmarks_config = (
+        ParameterBuilder("ur5e_cutter_moveit_config")
+        .yaml(
+            parameter_namespace="benchmark_config",
+            file_path="config/benchmarks.yaml",
+        )
+        .to_dict()
+    )
+
+    moveit_configs = (
+        MoveItConfigsBuilder("ur5e_cutter", package_name="ur5e_cutter_moveit_config")
+        .robot_description(
+            file_path="config/ur5e.urdf.xacro",
+            mappings={
+                "ros2_control_hardware_type": "isaac"
+            },
+        )
+        .planning_pipelines("ompl", ["ompl"]) #, "stomp", "pilz_industrial_motion_planner"])
+        .moveit_cpp(
+            os.path.join(
+                get_package_share_directory("ur5e_cutter_moveit_config"),
+                "config",
+                "benchmarking_moveit_cpp.yaml",
+            )
+        )
+        .to_moveit_configs()
+    )
+
+    warehouse_ros_config = {
+        "warehouse_plugin": "warehouse_ros_sqlite::DatabaseConnection", # needed to use sqlite instead of mongodb
+        # "warehouse_host": "localhost"  # not needed because host and port are set in benchmark_config
+    }
+
+    ompl_planning_pipeline_config = {
+        "planning_plugin": "ompl_interface/OMPLPlanner",    # currently a workaround for the Error: Multiple planning plugins available. You should specify the '~planning_plugin' parameter. Using 'chomp_interface/CHOMPPlanner' for now.
+    }
+
+    # moveit_ros_benchmark demo executable
+    moveit_ros_benchmarks_node = Node(
+        name="moveit_run_benchmark",
+        package="moveit_ros_benchmarks",
+        # prefix='xterm -e gdb --ex=run --args',
+        executable="moveit_run_benchmark",
+        output="screen",
+        parameters=[
+            moveit_ros_benchmarks_config,
+            moveit_configs.to_dict(),
+            warehouse_ros_config,
+            ompl_planning_pipeline_config,
+        ],
+    )
+
+    # Warehouse mongodb server
+    # mongodb_server_node = Node(
+    #     package="warehouse_ros_sqlite",
+    #     executable="mongo_wrapper_ros.py",
+    #     parameters=[
+    #         {"warehouse_port": 33829},
+    #         {"warehouse_host": "localhost"},
+    #         {"warehouse_plugin": "warehouse_ros_sqlit::DatabaseConnection"},
+    #     ],
+    #     output="screen",
+    # )
+
+    return LaunchDescription([moveit_ros_benchmarks_node]) #, mongodb_server_node])
+
